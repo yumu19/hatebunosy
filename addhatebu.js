f0bda3c3d105d9debeb85dff821cee2a111c0b9f
yumu19 <yumu.stp@gmail.com>
2013-02-02 21:06:57 +0900

    implement for first entry

--- addhatebu.js
+++ addhatebu.js
@@ -1,6 +1,6 @@
 $(function(){
-	$(".span12\ hotentry").each(function(){
-		//addhatebu(this);
+	$("[class^=span12]").each(function(){
+		addhatebu(this);
 	});
 	$(".span6").each(function(){ 
 		addhatebu(this);
@@ -11,14 +11,11 @@ function addhatebu(obj){
 	var entryurl = $("a:first",obj).attr("href").split("url=")[1];
 	var queryurl = "http://api.b.st-hatena.com/entry.count?url="+entryurl;
 	console.debug(decodeURIComponent(entryurl));
-	// $.get(queryurl,function(data){
- //    	console.log(data);
-	// });
+
 	$.ajax ({
 		url: queryurl,
 		success: function(data) {
-			var users = data;
-			var hatebu="<li><a href=\""+decodeURIComponent(entryurl)+"\">"+users+" users</a></li>";
+			var hatebu="<li><a href=\""+decodeURIComponent(entryurl)+"\" target=\"_blank\">"+data+" users</a></li>";
 			$(".entry-domain-for-article",obj).prepend(hatebu);
 		}
 	});
