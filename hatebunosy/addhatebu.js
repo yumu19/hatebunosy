16b5840dfb16b7b9e7e0c9c69d83d6a1a36e8ead
yumu19 <yumu.stp@gmail.com>
2013-02-03 18:35:08 +0900

    change link url to hatena bookmark

--- hatebunosy/addhatebu.js
+++ hatebunosy/addhatebu.js
@@ -8,20 +8,21 @@ $(function(){
 });
 
 function addhatebu(obj){
-	var entryurl = $("a:first",obj).attr("href").split("url=")[1];
-	var queryurl = "http://api.b.st-hatena.com/entry.count?url="+entryurl;
-	console.debug(decodeURIComponent(entryurl));
+	var entryurl_encode = $("a:first",obj).attr("href").split("url=")[1];
+	var entryurl_decode = decodeURIComponent(entryurl_encode)
+	var queryurl = "http://api.b.st-hatena.com/entry.count?url="+entryurl_encode;
+	var hatebuurl = "http://b.hatena.ne.jp/entry/"+entryurl_decode.replace("http://","");
 
 	$.ajax ({
 		url: queryurl,
 		success: function(data) {
 			if (data >= 10) {
-				var hatebu="<li><a href=\""+decodeURIComponent(entryurl)+"\" target=\"_blank\" class=\"hatebunosy10\"><strong>"
+				var hatebu="<li><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy10\"><strong>"
 						+data+" users</strong></a></li>";
 				$(".entry-domain-for-article",obj).prepend(hatebu);
 			}
 			else if (data >= 1) {
-				var hatebu="<li><a href=\""+decodeURIComponent(entryurl)+"\" target=\"_blank\" class=\"hatebunosy\"><strong>"
+				var hatebu="<li><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy\"><strong>"
 						+data+" users</a></strong></li>";
 				$(".entry-domain-for-article",obj).prepend(hatebu);
 
