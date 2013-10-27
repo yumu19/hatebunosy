ef5a6f3bac75871c2dc65d40e2833d8b1e17f9b7
yumu19 <yumu.stp@gmail.com>
2013-10-28 01:15:31 +0900

    fix layout

--- hatebunosy/addhatebu.js
+++ hatebunosy/addhatebu.js
@@ -24,18 +24,23 @@ function addhatebu(obj){
 	$.ajax ({
 		url: queryurl,
 		success: function(data) {
+			var hatebu_sub;
+			var hatebu_top;
 			if (data >= 10) {			
-				 var hatebu='<div class=\"hatebu\" ><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy10\" style=\"color:#FF0000\"><strong>'
-				  			 +data+' users</strong></a></div>';
-				$("span.rss.effect",obj).after(hatebu);
-				// $("div#main_art_text_wrap > div",obj).appendChild("<p>hogehoge<p>");
+				hatebu_sub="<div class=\"hatebu\" ><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy10\" style=\"color:#FF0000; margin:0 16px\"><strong>"
+				  			 +data+" users</strong></a></div>";
+				hatebu_top="<div class=\"hatebu\" ><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy10\" style=\"color:#FF0000\"><strong>"
+				  			 +data+" users</strong></a></div>";
+
 			}
 			else if (data >= 1) {
-				var hatebu="<div class=\"hatebu\" ><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy\" style=\"color:#FF6666\">"
+				hatebu_sub="<div class=\"hatebu\" ><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy\" style=\"color:#FF6666; margin:0 16px\">"
 				  			+data+" users</a></div>";
-				$("span.rss.effect",obj).after(hatebu);
-				// $("div#main_art_text_wrap div",obj).appendChild(hatebu);
+				hatebu_top="<div class=\"hatebu\" ><a href=\""+hatebuurl+"\" target=\"_blank\" class=\"hatebunosy\" style=\"color:#FF6666\">"
+				  			+data+" users</a></div>";
 			}
+			$("span.rss.effect",obj).after(hatebu_sub);
+			$("div#main_art_text_wrap > div",obj).prepend(hatebu_top);
 		}
 	});
 }
